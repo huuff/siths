7cbfe16f15d44dc4e7aa8bb4c5cb109867931772
Haf <haf@protonmail.ch>
2022-07-28 09:37:51 +0200

    publishing to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     kotlin("jvm") version "1.6.21"
     id("org.jetbrains.kotlinx.kover") version "0.5.0"
+    id("maven-publish")
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    id("signing")
 }
 
 group = "xyz.haff"
@@ -18,6 +21,63 @@ tasks.withType<Test> {
     useJUnitPlatform()
     jvmArgs("--add-opens", "java.base/java.time=ALL-UNNAMED")
     finalizedBy(tasks.koverHtmlReport)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                packaging = "jar"
+                name.set(project.name)
+                description.set("Small Redis utility library built on top of Jedis")
+
+                url.set("https://github.com/huuff/siths")
+                scm {
+                    connection.set("scm:git:git://github.com/huuff/siths.git")
+                    developerConnection.set("scm:git:git@github.com:huuff/siths.git")
+                    url.set("https://github.com/huuff/siths/tree/master")
+                }
+
+                licenses {
+                    license {
+                        name.set("WTFPL - Do What The Fuck You Want To Public License")
+                        url.set("http://www.wtfpl.net")
+                    }
+                }
+
+                developers {
+                    developer {
+                        name.set("Francisco Sánchez")
+                        email.set("haf@protonmail.ch")
+                        organizationUrl.set("https://github.com/huuff")
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(properties["sonatype.user"] as String)
+            password.set(properties["sonatype.password"] as String)
+        }
+    }
 }
 
 val kotestVersion = "5.3.1"
