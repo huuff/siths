7022a6c6a7b5963a994268f9c7e9c8ac2a54ee64
Haf <haf@protonmail.ch>
2022-08-03 14:38:10 +0200

    tested RedisCache

--- src/test/kotlin/xyz/haff/siths/RedisCacheTest.kt
+++ src/test/kotlin/xyz/haff/siths/RedisCacheTest.kt
@@ -5,6 +5,10 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.extensions.testcontainers.LifecycleMode
 import io.kotest.extensions.testcontainers.TestContainerExtension
 import io.kotest.matchers.shouldBe
+import io.mockk.spyk
+import io.mockk.verify
+import java.time.Duration
+import java.util.*
 
 // TODO: Actually implement it
 class RedisCacheTest : FunSpec({
@@ -12,5 +16,28 @@ class RedisCacheTest : FunSpec({
         withExposedPorts(6379)
     }
 
+    test("value is cached") {
+        // ARRANGE
+        val cache = RedisCache<String, String>(
+            lockTimeout = Duration.ofSeconds(10),
+            keyTtl = Duration.ofSeconds(10),
+            jedisPool = poolFromContainer(container),
+            serializingFunction = { it },
+            deserializingFunction = { it }
+        )
+        val loadingFunction = spyk( { UUID.randomUUID().toString() })
+
+        // ACT
+        val (firstResult, firstIsHit, firstKeyHash) = cache.getOrLoad("key") { loadingFunction() }
+        val (secondResult, secondIsHit, secondKeyHash) = cache.getOrLoad("key") { loadingFunction() }
+
+        // ASSERT
+        verify(exactly = 1) { loadingFunction() }
+
+        firstIsHit shouldBe false
+        secondIsHit shouldBe true
+        firstResult shouldBe secondResult
+        firstKeyHash shouldBe secondKeyHash
+    }
 
 })
