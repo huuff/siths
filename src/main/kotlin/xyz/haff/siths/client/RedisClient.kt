fcbee61332e1ac33f031cd743674469f456372ea
Haf <haf@protonmail.ch>
2022-08-17 18:01:06 +0200

    converting all data in a response from CLIENT LIST

--- src/main/kotlin/xyz/haff/siths/client/RedisClient.kt
+++ src/main/kotlin/xyz/haff/siths/client/RedisClient.kt
@@ -2,10 +2,9 @@ package xyz.haff.siths.client
 
 import io.ktor.network.sockets.*
 
-// TODO: Store everything?
 data class RedisClient(
     val id: String,
-    val name: String,
+    val name: String?,
     val addr: InetSocketAddress,
     val laddr: InetSocketAddress,
     val fd: Int,
@@ -33,51 +32,49 @@ data class RedisClient(
     val user: String,
     val redir: Int,
     val resp: Int,
-)
+) {
+    companion object {
+        fun fromString(string: String) = string.split(SPACES_REGEX)
+            .associate {
+                val (key, value) = it.split("=")
+                Pair(key, value.ifEmpty { null })
+            }.let { map -> RedisClient(
+                    id = map["id"]!!,
+                    addr = stringToAddr(map["addr"]!!),
+                    laddr = stringToAddr(map["laddr"]!!),
+                    fd = map["fd"]!!.toInt(),
+                    name = map["name"],
+                    age = map["age"]!!.toInt(),
+                    idle = map["idle"]!!.toInt(),
+                    flags = map["flags"]!!,
+                    db = map["db"]!!.toInt(),
+                    sub = map["sub"]!!.toInt(),
+                    psub = map["psub"]!!.toInt(),
+                    ssub = map["ssub"]!!.toInt(),
+                    multi = map["multi"]!!.toInt(),
+                    qbuf = map["qbuf"]!!.toInt(),
+                    qbufFree = map["qbuf-free"]!!.toInt(),
+                    argvMem = map["argv-mem"]!!.toInt(),
+                    multiMem = map["multi-mem"]!!.toInt(),
+                    rbs = map["rbs"]!!.toInt(),
+                    rbp = map["rbp"]!!.toInt(),
+                    obl = map["obl"]!!.toInt(),
+                    oll = map["oll"]!!.toInt(),
+                    omem = map["omem"]!!.toInt(),
+                    totMem = map["tot-mem"]!!.toInt(),
+                    events = map["events"]!!,
+                    cmd = map["cmd"]!!,
+                    user = map["user"]!!,
+                    redir = map["redir"]!!.toInt(),
+                    resp = map["resp"]!!.toInt()
+                )
+            }
+    }
+}
 
 private fun stringToAddr(string: String) = string.split(":").let { (ip, port) -> InetSocketAddress(ip, port.toInt()) }
 private val SPACES_REGEX = Regex("\\s+")
 // TODO: Test
 fun parseClientList(response: String): List<RedisClient> = response.split("\n")
-    .asSequence()
     .filter { it.isNotBlank() }
-    .map { it.split(SPACES_REGEX)}
-    .map { pairs ->
-        mutableMapOf<String, String>().apply {
-            pairs.forEach { pair ->
-                this +=  pair.split("=").let { (key, value) -> key to value } // TODO: What to do with empty fields?
-            }
-        }
-    }
-    .map { map -> RedisClient(
-        id = map["id"]!!,
-        addr = stringToAddr(map["addr"]!!),
-        laddr = stringToAddr(map["laddr"]!!),
-        fd = map["fd"]!!.toInt(),
-        name = map["name"]!!,
-        age = map["age"]!!.toInt(),
-        idle = map["idle"]!!.toInt(),
-        flags = map["flags"]!!,
-        db = map["db"]!!.toInt(),
-        sub = map["sub"]!!.toInt(),
-        psub = map["psub"]!!.toInt(),
-        ssub = map["ssub"]!!.toInt(),
-        multi = map["multi"]!!.toInt(),
-        qbuf = map["qbuf"]!!.toInt(),
-        qbufFree = map["qbuf-free"]!!.toInt(),
-        argvMem = map["argv-mem"]!!.toInt(),
-        multiMem = map["multi-mem"]!!.toInt(),
-        rbs = map["rbs"]!!.toInt(),
-        rbp = map["rbp"]!!.toInt(),
-        obl = map["obl"]!!.toInt(),
-        oll = map["oll"]!!.toInt(),
-        omem = map["omem"]!!.toInt(),
-        totMem = map["tot-mem"]!!.toInt(),
-        events = map["events"]!!,
-        cmd = map["cmd"]!!,
-        user = map["user"]!!,
-        redir = map["redir"]!!.toInt(),
-        resp = map["resp"]!!.toInt()
-    )
-    }
-    .toList()
+    .map { RedisClient.fromString(it) }
---
+++ src/test/kotlin/xyz/haff/siths/client/RedisClientTest.kt
@@ -0,0 +1,49 @@
+package xyz.haff.siths.client
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.ktor.network.sockets.*
+
+class RedisClientTest : FunSpec({
+
+    test("correctly parses string") {
+        // ARRANGE
+        val clientListResponse = "id=18 addr=172.17.0.1:56132 laddr=172.17.0.2:6379 fd=9 name= age=25639 idle=0 flags=N db=0 sub=0 psub=0 ssub=0 multi=-1 qbuf=13 qbuf-free=20461 argv-mem=10 multi-mem=0 rbs=1024 rbp=0 obl=0 oll=0 omem=0 tot-mem=22298 events=r cmd=client|list user=default redir=-1 resp=2"
+
+        // ACT
+        val client = RedisClient.fromString(clientListResponse)
+
+        // ASSERT
+        client shouldBe RedisClient(
+            id = "18",
+            addr = InetSocketAddress("172.17.0.1", 56132),
+            laddr = InetSocketAddress("172.17.0.2", 6379),
+            fd = 9,
+            name = null,
+            age = 25639,
+            idle = 0,
+            flags = "N",
+            db = 0,
+            sub = 0,
+            psub = 0,
+            ssub = 0,
+            multi = -1,
+            qbuf = 13,
+            qbufFree = 20461,
+            argvMem = 10,
+            multiMem = 0,
+            rbs = 1024,
+            rbp = 0,
+            obl = 0,
+            oll = 0,
+            omem = 0,
+            totMem = 22298,
+            events = "r",
+            cmd = "client|list",
+            user = "default",
+            redir = -1,
+            resp = 2,
+        )
+    }
+
+})
