b19e1a4e4c7bff90535811bc5856929b1a5425ac
Haf <haf@protonmail.ch>
2022-08-20 10:59:24 +0200

    tested set removal

--- src/test/kotlin/xyz/haff/siths/client/SithsSetTest.kt
+++ src/test/kotlin/xyz/haff/siths/client/SithsSetTest.kt
@@ -3,6 +3,7 @@ package xyz.haff.siths.client
 import io.kotest.core.extensions.install
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.extensions.testcontainers.TestContainerExtension
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 import xyz.haff.siths.makeSithsPool
 
@@ -23,6 +24,23 @@ class SithsSetTest : FunSpec({
         set.size shouldBe 2
         ("key1" in set) shouldBe true
         ("key2" in set) shouldBe true
+    }
+
+    test("can remove elements") {
+        // ARRANGE
+        val set = SithsSet<String>(sithsPool = makeSithsPool(container))
+        set += "key1"
+
+        // SANITY CHECK
+        ("key1" in set) shouldBe true
+        set.size shouldBe 1
+
+        // ACT
+        set -= "key1"
+
+        // ASSERT
+        ("key1" in set) shouldBe false
+        set.size shouldBe 0
     }
 
 })
