4a5e6324748b6fc7fcdf86d7e74de27faf0c8c93
Haf <haf@protonmail.ch>
2022-08-20 11:25:52 +0200

    typed headAndTail

--- src/main/kotlin/xyz/haff/siths/common/Util.kt
+++ src/main/kotlin/xyz/haff/siths/common/Util.kt
@@ -6,4 +6,4 @@ fun randomUUID() = UUID.randomUUID().toString()
 
 // TODO: This in koy
 // OPT: Prevent copying the array
-fun <T> Array<T>.headAndTail() = Pair(this[0], this.copyOfRange(1, this.size))
+fun <T> Array<T>.headAndTail(): Pair<T, Array<T>> = Pair(this[0], this.copyOfRange(1, this.size))
