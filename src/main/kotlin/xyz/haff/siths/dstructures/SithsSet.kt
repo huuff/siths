140b0aa6397f2686336da8b3d4fd5359cdddb177
Haf <haf@protonmail.ch>
2022-09-02 12:11:10 +0200

    removed todo, it's better this way

--- src/main/kotlin/xyz/haff/siths/dstructures/SithsSet.kt
+++ src/main/kotlin/xyz/haff/siths/dstructures/SithsSet.kt
@@ -100,33 +100,4 @@ class SithsSet<T : Any>(
     }
 
     override fun isEmpty(): Boolean = size == 0
-
-    inner class Iterator(
-        private var lastCursorResult: RedisCursor<T>,
-    ) : MutableIterator<T> {
-        private var positionWithinLastCursor = 0
-        private var lastResult: T? =
-            null // XXX: Only to implement `remove`... it's hacky but all of my other options were too
-
-        override fun hasNext(): Boolean =
-            lastCursorResult.next != 0L || positionWithinLastCursor < lastCursorResult.contents.size
-
-        override fun next(): T = lastCursorResult.contents[positionWithinLastCursor].also {
-            lastResult = it
-            positionWithinLastCursor++
-            // XXX: Overflowed the last cursor, but there are more elements
-            if (positionWithinLastCursor >= lastCursorResult.contents.size && hasNext()) {
-                lastCursorResult = runBlocking { client.sscan(name, lastCursorResult.next).map(deserialize) }
-                positionWithinLastCursor = 0
-            }
-        }
-
-        override fun remove() {
-            if (lastResult != null) {
-                // XXX: Non-null assertion is correct, since if `lastResult` has changed, it must have changed to a non-null value
-                // (see `next()`)
-                runBlocking { client.srem(name, serialize(lastResult!!)) }
-            }
-        }
-    }
 }
