5ba2266cd15c6c69ff3417fa2f1db92a5cc20f89
Haf <haf@protonmail.ch>
2022-11-30 14:03:19 +0100

    a better exception to notify that there was an error healing the pool

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "xyz.haff"
-version = "0.12.1"
+version = "0.12.2"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/xyz/haff/siths/pooling/DefaultPool.kt
+++ src/main/kotlin/xyz/haff/siths/pooling/DefaultPool.kt
@@ -7,36 +7,45 @@ import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
 class ExhaustedPoolException(maxResources: Int) : RuntimeException("This pool has all $maxResources resources busy")
+class PoolHealingException(e: ConcurrentModificationException) :
+    RuntimeException("Failed healing pool because it was modified concurrently", e)
 
-class DefaultPool<ResourceType, PooledResourceType: PooledResource<ResourceType>>(
+class DefaultPool<ResourceType, PooledResourceType : PooledResource<ResourceType>>(
     private val acquireTimeout: Duration = 10.seconds,
     private val maxResources: Int = 10,
     private val createNewResource: suspend (Pool<ResourceType, PooledResourceType>) -> PooledResourceType,
-): Pool<ResourceType, PooledResourceType> {
+) : Pool<ResourceType, PooledResourceType> {
     private val resources = mutableMapOf<String, PooledResourceType>()
     private val mutex = Mutex()
 
     override val currentResources get() = resources.values.count { it.status != PoolStatus.BROKEN }
-    
+
     override suspend fun get(): PooledResourceType {
         val deadline = System.currentTimeMillis() + acquireTimeout.inWholeMilliseconds
 
         while (System.currentTimeMillis() < deadline) {
             mutex.withLock {
                 // First, clean all broken resources
-                resources.forEach { (key, value) -> if (value.status == PoolStatus.BROKEN) { remove(key) } }
+                try {
+                    resources.forEach { (key, value) ->
+                        if (value.status == PoolStatus.BROKEN) {
+                            remove(key)
+                        }
+                    }
+                } catch (e: ConcurrentModificationException) {
+                    throw PoolHealingException(e)
+                }
+
 
                 if (resources.values.any { it.status == PoolStatus.FREE }) {
                     val connection = resources.values.find { it.status == PoolStatus.FREE }!!
                     connection.status = PoolStatus.BUSY
                     return connection
-                } else {
-                    if (currentResources < maxResources) {
-                        val connection = createNewResource(this)
-                        connection.status = PoolStatus.BUSY
-                        resources[connection.identifier] = connection
-                        return connection
-                    }
+                } else if (currentResources < maxResources) {
+                    val connection = createNewResource(this)
+                    connection.status = PoolStatus.BUSY
+                    resources[connection.identifier] = connection
+                    return connection
                 }
             }
             delay(10L)
